b66369c1934a3761d60caa1c0ccbcf8967b54924
Anderson Ribeiro <andersonribeiro.si@hotmail.com>
2020-02-11 19:49:59 -0300

    Implementação do teste de validar email no serviço de usuário

---
+++ src/test/java/com/andersonribeiro/minhasfinancas/service/UsuarioServiceTest.java
@@ -0,0 +1,43 @@
+package com.andersonribeiro.minhasfinancas.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.andersonribeiro.minhasfinancas.exceptions.RegraNegocioException;
+import com.andersonribeiro.minhasfinancas.model.entity.Usuario;
+import com.andersonribeiro.minhasfinancas.model.repository.UsuarioRepository;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class UsuarioServiceTest {
+	
+	@Autowired
+	UsuarioService service;
+	
+	@Autowired
+	UsuarioRepository repository;
+	
+	@Test(expected = Test.None.class) // Quando um método retorna uma exception, com essa anotação expected = Test.None.class, não sera retornada nenhuma exception
+	public void deveValidarEmail() {
+		
+		//Cenário
+		repository.deleteAll();
+		
+		//Ação // Execução
+		service.validarEmail("anderson@gmail.com");
+			
+	}
+	
+	@Test(expected = RegraNegocioException.class) // Aqui o teste espera que seja lançanda a exception RegraNegocioException
+	public void deveLancarErroAoValidarEmailQuandoExistirEmailCadastrado() {
+		//Cenário
+		Usuario usuario = Usuario.builder().nome("usuario").email("andersonribeiro.sifacisa@gmail.com").build();
+		repository.save(usuario);
+		
+		//Ação
+		service.validarEmail("andersonribeiro.sifacisa@gmail.com");
+	}
+}
