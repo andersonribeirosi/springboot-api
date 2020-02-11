1dc0cd953e0be90b45c1c5ce480e1446964b4dfd
Anderson Ribeiro <andersonribeiro.si@hotmail.com>
2020-02-11 17:43:26 -0300

    Adicionado a dependencia do H2 database para realização de testes dos métodos implementados

--- src/test/java/com/andersonribeiro/minhasfinancas/model/repository/UsuarioRepositoryTest.java
+++ src/test/java/com/andersonribeiro/minhasfinancas/model/repository/UsuarioRepositoryTest.java
@@ -4,13 +4,16 @@ import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.andersonribeiro.minhasfinancas.model.entity.Usuario;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
+//@ActiveProfiles("test") // Vai buscar o application-test-properties (h2 in memory)
 public class UsuarioRepositoryTest {
 	
 	@Autowired
@@ -20,11 +23,11 @@ public class UsuarioRepositoryTest {
 	public void verificarEmail() {
 		
 		// Cenário
-		Usuario usuario = Usuario.builder().nome("usuario").email("usuario@email.com").build();
+		Usuario usuario = Usuario.builder().nome("usuario").email("and@email.com").build();
 		repository.save(usuario);
 	
 		// açao / execução
-		boolean result = repository.existsByEmail("usuario@email.com");
+		boolean result = repository.existsByEmail("and@email.com");
 	
 		// verificação
 		Assertions.assertThat(result).isTrue();
