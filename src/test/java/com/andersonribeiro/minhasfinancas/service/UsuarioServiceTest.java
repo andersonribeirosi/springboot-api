5bd75ddb3050dda63ec891841f0c94b65652a757
Anderson <andersonribeiro.si@hotmail.com>
2020-02-16 16:00:15 -0300

    Adicionando mocks nos testes através do mockito de UsuarioService

--- src/test/java/com/andersonribeiro/minhasfinancas/service/UsuarioServiceTest.java
+++ src/test/java/com/andersonribeiro/minhasfinancas/service/UsuarioServiceTest.java
@@ -1,7 +1,9 @@
 package com.andersonribeiro.minhasfinancas.service;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -9,11 +11,43 @@ import org.springframework.test.context.junit4.SpringRunner;
 import com.andersonribeiro.minhasfinancas.exceptions.RegraNegocioException;
 import com.andersonribeiro.minhasfinancas.model.entity.Usuario;
 import com.andersonribeiro.minhasfinancas.model.repository.UsuarioRepository;
+import com.andersonribeiro.minhasfinancas.service.impl.UsuarioServiceImpl;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
 public class UsuarioServiceTest {
 	
+	UsuarioService service;
+	UsuarioRepository repository;
+	
+	@Before
+	public void setUp() {
+		repository = Mockito.mock(UsuarioRepository.class);
+		service = new UsuarioServiceImpl(repository);
+	}
+	
+	@Test(expected = Test.None.class) // Quando um método retorna uma exception, com essa anotação expected = Test.None.class, não sera retornada nenhuma exception
+	public void deveValidarEmail() {
+
+		//Cenário
+		Mockito.when(repository.existsByEmail(Mockito.anyString())).thenReturn(false);
+		
+		//Ação // Execução
+		service.validarEmail("anderson@gmail.com");
+			
+	}
+	
+	@Test(expected = RegraNegocioException.class) // Aqui o teste espera que seja lançanda a exception RegraNegocioException
+	public void deveLancarErroAoValidarEmailQuandoExistirEmailCadastrado() {
+		//Cenário
+		Mockito.when(repository.existsByEmail(Mockito.anyString())).thenReturn(true);
+		
+		//Ação
+		service.validarEmail("andersonribeiro.sifacisa@gmail.com");
+	}
+	
+}
+	/**
 	@Autowired
 	UsuarioService service;
 	
@@ -41,3 +75,4 @@ public class UsuarioServiceTest {
 		service.validarEmail("andersonribeiro.sifacisa@gmail.com");
 	}
 }
+**/
