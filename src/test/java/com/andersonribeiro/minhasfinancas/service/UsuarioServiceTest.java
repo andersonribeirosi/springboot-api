be9fcf4f94ed67749b13b8d331c2348a1374ea9d
Anderson <andersonribeiro.si@hotmail.com>
2020-02-16 17:45:14 -0300

    Finalizado os testes de autenticação utilizando o Mockito

--- src/test/java/com/andersonribeiro/minhasfinancas/service/UsuarioServiceTest.java
+++ src/test/java/com/andersonribeiro/minhasfinancas/service/UsuarioServiceTest.java
@@ -1,13 +1,17 @@
 package com.andersonribeiro.minhasfinancas.service;
 
+import java.util.Optional;
+
+import org.assertj.core.api.Assertions;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.andersonribeiro.minhasfinancas.exceptions.ErroAutenticacaoException;
 import com.andersonribeiro.minhasfinancas.exceptions.RegraNegocioException;
 import com.andersonribeiro.minhasfinancas.model.entity.Usuario;
 import com.andersonribeiro.minhasfinancas.model.repository.UsuarioRepository;
@@ -16,63 +20,109 @@ import com.andersonribeiro.minhasfinancas.service.impl.UsuarioServiceImpl;
 @SpringBootTest
 @RunWith(SpringRunner.class)
 public class UsuarioServiceTest {
-	
+
 	UsuarioService service;
+
+	@MockBean // Cria toda a instancia da linha 28
 	UsuarioRepository repository;
-	
+
 	@Before
 	public void setUp() {
-		repository = Mockito.mock(UsuarioRepository.class);
+//		repository = Mockito.mock(UsuarioRepository.class); 
 		service = new UsuarioServiceImpl(repository);
 	}
-	
-	@Test(expected = Test.None.class) // Quando um método retorna uma exception, com essa anotação expected = Test.None.class, não sera retornada nenhuma exception
+
+	@Test(expected = Test.None.class)
+	public void deveAutenticarUmUsuarioComSucesso() {
+		// Cenário
+		String email = "anderson@email.com";
+		String senha = "senha";
+
+		Usuario usuario = Usuario.builder().email(email).senha(senha).id(1l).build();
+		Mockito.when(repository.findByEmail(email)).thenReturn(Optional.of(usuario));
+
+		// Ação
+		Usuario result = service.autenticar(email, senha);
+
+		// Verificação
+		Assertions.assertThat(result).isNotNull();
+
+	}
+
+	@Test
+	public void deveLancarErroQuandoNaoEncontrarUsuarioParaOemailInformado() {
+		// Cenário
+		Mockito.when(repository.findByEmail(Mockito.anyString())).thenReturn(Optional.empty());
+
+		// Ação
+		Throwable exception = Assertions.catchThrowable(() -> service.autenticar("anderson@gmail.com", "123456"));
+
+		// Verificação
+		Assertions.assertThat(exception).isInstanceOf(ErroAutenticacaoException.class)
+				.hasMessage("Usuario não encontrado para este email");
+	}
+
+	@Test
+	public void deveLancarErroQuandoAsenhaNaoBater() {
+		// Cenário
+		String senha = "senha";
+		Usuario usuario = Usuario.builder().email("email@email.com").senha(senha).build();
+		Mockito.when(repository.findByEmail(Mockito.anyString())).thenReturn(Optional.of(usuario));
+
+		// Ação
+		Throwable exception = Assertions.catchThrowable(() -> service.autenticar("email@email.com", "1904"));
+
+		// Verificação
+		Assertions.assertThat(exception).isInstanceOf(ErroAutenticacaoException.class)
+				.hasMessage("Senha inválida para o email informado");
+	}
+
+	@Test(expected = Test.None.class) // Quando um método retorna uma exception, com essa anotação expected =
+										// Test.None.class, não sera retornada nenhuma exception
 	public void deveValidarEmail() {
 
-		//Cenário
+		// Cenário
 		Mockito.when(repository.existsByEmail(Mockito.anyString())).thenReturn(false);
-		
-		//Ação // Execução
+
+		// Ação // Execução
 		service.validarEmail("anderson@gmail.com");
-			
+
 	}
-	
-	@Test(expected = RegraNegocioException.class) // Aqui o teste espera que seja lançanda a exception RegraNegocioException
+
+	@Test(expected = RegraNegocioException.class) // Aqui o teste espera que seja lançanda a exception
+													// RegraNegocioException
 	public void deveLancarErroAoValidarEmailQuandoExistirEmailCadastrado() {
-		//Cenário
+		// Cenário
 		Mockito.when(repository.existsByEmail(Mockito.anyString())).thenReturn(true);
-		
-		//Ação
+
+		// Ação
 		service.validarEmail("andersonribeiro.sifacisa@gmail.com");
 	}
-	
+
 }
-	/**
-	@Autowired
-	UsuarioService service;
-	
-	@Autowired
-	UsuarioRepository repository;
-	
-	@Test(expected = Test.None.class) // Quando um método retorna uma exception, com essa anotação expected = Test.None.class, não sera retornada nenhuma exception
-	public void deveValidarEmail() {
-		
-		//Cenário
-		repository.deleteAll();
-		
-		//Ação // Execução
-		service.validarEmail("anderson@gmail.com");
-			
-	}
-	
-	@Test(expected = RegraNegocioException.class) // Aqui o teste espera que seja lançanda a exception RegraNegocioException
-	public void deveLancarErroAoValidarEmailQuandoExistirEmailCadastrado() {
-		//Cenário
-		Usuario usuario = Usuario.builder().nome("usuario").email("andersonribeiro.sifacisa@gmail.com").build();
-		repository.save(usuario);
-		
-		//Ação
-		service.validarEmail("andersonribeiro.sifacisa@gmail.com");
-	}
-}
-**/
+/**
+ * @Autowired UsuarioService service;
+ * 
+ * @Autowired UsuarioRepository repository;
+ * 
+ * @Test(expected = Test.None.class) // Quando um método retorna uma exception,
+ *                com essa anotação expected = Test.None.class, não sera
+ *                retornada nenhuma exception public void deveValidarEmail() {
+ * 
+ *                //Cenário repository.deleteAll();
+ * 
+ *                //Ação // Execução service.validarEmail("anderson@gmail.com");
+ * 
+ *                }
+ * 
+ * @Test(expected = RegraNegocioException.class) // Aqui o teste espera que seja
+ *                lançanda a exception RegraNegocioException public void
+ *                deveLancarErroAoValidarEmailQuandoExistirEmailCadastrado() {
+ *                //Cenário Usuario usuario =
+ *                Usuario.builder().nome("usuario").email("andersonribeiro.sifacisa@gmail.com").build();
+ *                repository.save(usuario);
+ * 
+ *                //Ação
+ *                service.validarEmail("andersonribeiro.sifacisa@gmail.com"); }
+ *                }
+ **/
