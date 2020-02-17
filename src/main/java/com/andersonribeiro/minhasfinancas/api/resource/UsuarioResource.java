965ab807f6bd1bc3bb0a89a9c4ca898f1518351d
Anderson <andersonribeiro.si@hotmail.com>
2020-02-17 15:42:28 -0300

    Criação do DTO de usuarios e implementação do post de usuarios

--- src/main/java/com/andersonribeiro/minhasfinancas/api/resource/UsuarioResource.java
+++ src/main/java/com/andersonribeiro/minhasfinancas/api/resource/UsuarioResource.java
@@ -1,14 +1,42 @@
 package com.andersonribeiro.minhasfinancas.api.resource;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.andersonribeiro.minhasfinancas.api.dto.UsuarioDTO;
+import com.andersonribeiro.minhasfinancas.exceptions.RegraNegocioException;
+import com.andersonribeiro.minhasfinancas.model.entity.Usuario;
+import com.andersonribeiro.minhasfinancas.service.UsuarioService;
+
 @RestController
+@RequestMapping("/api/usuarios")
 public class UsuarioResource {
 	
-	@GetMapping("/")
-	public String helloWorld() {
-		return "Hello World";
+	private UsuarioService service;
+	
+	public UsuarioResource(UsuarioService service) {
+		this.service = service;
+	}
+	
+	//@ResposeEntity - Representa o corpo da resposta
+	//@RequestBody - Retorna apenas o que contem no DTO
+	@PostMapping
+	public ResponseEntity salvar(@RequestBody UsuarioDTO dto) {
+		
+		Usuario usuario = Usuario.builder()
+				.nome(dto.getNome())
+				.email(dto.getEmail())
+				.senha(dto.getSenha()).build();
+	try {
+		Usuario usuarioSalvo = service.salvarUsuario(usuario);
+		return new ResponseEntity(usuarioSalvo, HttpStatus.CREATED);
+	} catch (RegraNegocioException e) {
+		return ResponseEntity.badRequest().body(e.getMessage());
+	}	
 	}
 
 }
