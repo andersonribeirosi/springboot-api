d48d0f1c7dbb7fb1c96170fd797ecc6076c4ea6d
Anderson <andersonribeiro.si@hotmail.com>
2020-02-17 22:53:02 -0300

    Implementado os métodos de atualizar, deletar e alterar status do lançamento

---
+++ src/main/java/com/andersonribeiro/minhasfinancas/service/impl/LancamentoServiceImpl.java
@@ -0,0 +1,61 @@
+package com.andersonribeiro.minhasfinancas.service.impl;
+
+import java.util.List;
+import java.util.Objects;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.andersonribeiro.minhasfinancas.model.entity.Lancamento;
+import com.andersonribeiro.minhasfinancas.model.enums.StatusLancamento;
+import com.andersonribeiro.minhasfinancas.model.repository.LancamentoRepository;
+import com.andersonribeiro.minhasfinancas.service.LancamentoService;
+
+@Service
+public class LancamentoServiceImpl implements LancamentoService {
+
+	private LancamentoRepository repository;
+	
+	public LancamentoServiceImpl(LancamentoRepository repository) {
+		this.repository = repository;
+	}
+	
+	@Override
+	@Transactional
+	public Lancamento salvar(Lancamento lancamento) {
+		return repository.save(lancamento);
+	}
+
+	@Override
+	@Transactional
+	public Lancamento atualizar(Lancamento lancamento) {
+		Objects.requireNonNull(lancamento.getId());
+		return repository.save(lancamento);
+	}
+
+	@Override
+	@Transactional
+	public void deletar(Lancamento lancamento) {
+		Objects.requireNonNull(lancamento.getId());
+		repository.delete(lancamento);
+	}
+
+	@Override
+	public List<Lancamento> buscar(Lancamento lancamentoFiltro) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void atualizarStatus(Lancamento lancamento, StatusLancamento status) {
+		lancamento.setStatus(status);
+		atualizar(lancamento);
+	}
+
+	@Override
+	public void validar(Lancamento lancamento) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
