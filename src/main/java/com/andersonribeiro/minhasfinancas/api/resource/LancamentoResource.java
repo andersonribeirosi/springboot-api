6e191d5c967466803dbcdfdf23c1ab60bc79b5a5
Anderson <andersonribeiro.si@hotmail.com>
2020-02-19 00:05:28 -0300

    Implementação do endpoint de buscar/filtrar lançamentos

--- src/main/java/com/andersonribeiro/minhasfinancas/api/resource/LancamentoResource.java
+++ src/main/java/com/andersonribeiro/minhasfinancas/api/resource/LancamentoResource.java
@@ -1,13 +1,18 @@
 package com.andersonribeiro.minhasfinancas.api.resource;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.andersonribeiro.minhasfinancas.api.dto.LancamentoDTO;
@@ -63,6 +68,30 @@ public class LancamentoResource {
 			return new ResponseEntity(HttpStatus.NO_CONTENT);
 		}).orElseGet(() -> 
 		new ResponseEntity("Lançamento não encontrado na base de dados.", HttpStatus.BAD_REQUEST));
+	}
+	
+	@GetMapping
+	public ResponseEntity buscar(
+			@RequestParam (value = "descricao", required = false) String descricao,
+			@RequestParam (value = "mes", required = false) Integer mes,
+			@RequestParam (value = "ano", required = false) Integer ano,
+			@RequestParam (value = "usuario") Long idUsuario
+			) {
+		Lancamento lancamentoFiltro = new Lancamento();
+		lancamentoFiltro.setDescricao(descricao);
+		lancamentoFiltro.setMes(mes);
+		lancamentoFiltro.setAno(ano);
+		
+		Optional<Usuario> usuario = usuarioService.obterUsuarioPorId(idUsuario);
+		if(usuario.isPresent()) {
+			return ResponseEntity.badRequest().body("Não foi possível realizar a consulta");
+		}else {
+			lancamentoFiltro.setUsuario(usuario.get());
+		}
+		
+		List<Lancamento> lancamentos = service.buscar(lancamentoFiltro);
+		return ResponseEntity.ok(lancamentos);
+				
 	}
 
 	private Lancamento converter(LancamentoDTO dto) {
