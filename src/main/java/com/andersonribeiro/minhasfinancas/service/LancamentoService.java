85e089d792dbe2c09d7697a6fb7954394eb03748
Anderson <andersonribeiro.si@hotmail.com>
2020-02-18 23:14:48 -0300

    Implementação dos endpoints de salvar e atualizar lançamentos

--- src/main/java/com/andersonribeiro/minhasfinancas/service/LancamentoService.java
+++ src/main/java/com/andersonribeiro/minhasfinancas/service/LancamentoService.java
@@ -1,6 +1,7 @@
 package com.andersonribeiro.minhasfinancas.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.andersonribeiro.minhasfinancas.model.entity.Lancamento;
 import com.andersonribeiro.minhasfinancas.model.enums.StatusLancamento;
@@ -18,5 +19,6 @@ public interface LancamentoService {
 	void atualizarStatus(Lancamento lancamento, StatusLancamento status);
 	
 	void validar(Lancamento lancamento);
-
+	
+	Optional<Lancamento> obterPorId(Long id);
 }
