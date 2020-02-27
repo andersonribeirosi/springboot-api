50e0ca02371aba976758af77fdcb3a0459975522
Anderson <andersonribeiro.si@hotmail.com>
2020-02-27 00:19:35 -0300

    Implementação do enpoint de buscar o lançamento por id

--- src/main/java/com/andersonribeiro/minhasfinancas/api/controllers/LancamentoController.java
+++ src/main/java/com/andersonribeiro/minhasfinancas/api/controllers/LancamentoController.java
@@ -47,6 +47,13 @@ public class LancamentoController {
 			return ResponseEntity.badRequest().body(e.getMessage());
 		}
 	}
+	
+	@GetMapping("{id}")
+	public ResponseEntity obterLancamento(@PathVariable("id") Long id) {
+		return service.obterPorId(id)
+				.map(lancamento -> new ResponseEntity(converter(lancamento), HttpStatus.OK))
+				.orElseGet(() -> new ResponseEntity(HttpStatus.NOT_FOUND));
+	}
 
 	@PutMapping("{id}")
 	public ResponseEntity atualizar(@PathVariable("id") Long id, @RequestBody LancamentoDTO dto) {
@@ -110,6 +117,20 @@ public class LancamentoController {
 				return ResponseEntity.badRequest().body(e.getMessage());
 			}
 		}).orElseGet(() -> new ResponseEntity("Lançamento não encontrado na base de dados", HttpStatus.BAD_REQUEST));
+	}
+	
+	
+	private LancamentoDTO converter(Lancamento lancamento) {
+		return LancamentoDTO.builder()
+				.id(lancamento.getId())
+				.descricao(lancamento.getDescricao())
+				.valor(lancamento.getValor())
+				.mes(lancamento.getMes())
+				.ano(lancamento.getAno())
+				.status(lancamento.getStatus().name())
+				.tipo(lancamento.getTipo().name())
+				.usuario(lancamento.getUsuario().getId())
+				.build();
 	}
 
 	private Lancamento converter(LancamentoDTO dto) {
--- src/main/java/com/andersonribeiro/minhasfinancas/api/dto/LancamentoDTO.java
+++ src/main/java/com/andersonribeiro/minhasfinancas/api/dto/LancamentoDTO.java
@@ -2,11 +2,17 @@ package com.andersonribeiro.minhasfinancas.api.dto;
 
 import java.math.BigDecimal;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class LancamentoDTO {
 
 	private Long id;
